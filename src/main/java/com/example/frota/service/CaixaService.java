b7f2b653a25700971a877b09fc6a8ea5759a6634
JM <oshirohiro00@gmail.com>
2025-10-29 22:35:33 -0300

    Update CaixaService.java

--- src/main/java/com/example/frota/service/CaixaService.java
+++ src/main/java/com/example/frota/service/CaixaService.java
@@ -1,13 +1,29 @@
 package com.example.frota.service;
 
-import com.example.frota.repository.CaixaRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.frota.entity.Caixa;
+import com.example.frota.entity.Produto;
+import com.example.frota.repository.CaixaRepository;
 
+@Service
 public class CaixaService {
-
+	
     @Autowired
-    private CaixaRepository repository;
+	private CaixaRepository caixaRepository;
 
 
+    public Caixa salvar(Caixa caixa) {
+		return caixaRepository.save(caixa);
+	}
+	public void apagarPorId (Long id) {
+		caixaRepository.deleteById(id);
+	}
+    public Caixa escolherCaixaMaisProxima(Produto produto) {
+    	return caixaRepository.findCaixaMaisProxima(produto.getComprimento(), produto.getLargura(), produto.getAltura(), produto.getPeso());
+    }
+    
+    
 }
