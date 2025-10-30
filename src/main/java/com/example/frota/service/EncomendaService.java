265542d295af8008694e40a479cc1a078e4d34c3
JM <oshirohiro00@gmail.com>
2025-10-30 01:42:19 -0300

    Create EncomendaService.java

---
+++ src/main/java/com/example/frota/service/EncomendaService.java
@@ -0,0 +1,33 @@
+package com.example.frota.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.frota.entity.Encomenda;
+import com.example.frota.repository.EncomendaRepository;
+
+@Service
+public class EncomendaService {
+	
+	@Autowired
+	private EncomendaRepository encomendaRepository;
+	
+	public Encomenda salvar(Encomenda encomenda) {
+		return encomendaRepository.save(encomenda);
+	}
+	
+	public List<Encomenda> procurarTodos(){
+		return encomendaRepository.findAll();
+	}
+	public void apagarPorId (Long id) {
+		encomendaRepository.deleteById(id);
+	}
+	
+	public Optional<Encomenda> procurarPorId(Long id) {
+		return encomendaRepository.findById(id);
+	}
+	    
+}
