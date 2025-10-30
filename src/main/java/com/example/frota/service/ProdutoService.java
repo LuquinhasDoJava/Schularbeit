bf93c044e7b1b2cc50ff81b85f9a0465a56f63df
JM <oshirohiro00@gmail.com>
2025-10-30 01:40:56 -0300

    Create ProdutoService.java

---
+++ src/main/java/com/example/frota/service/ProdutoService.java
@@ -0,0 +1,32 @@
+package com.example.frota.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.frota.dto.AtualizacaoProduto;
+import com.example.frota.entity.Produto;
+import com.example.frota.repository.ProdutoRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
+	
+	public Produto salvar(Produto produto) {
+		return produtoRepository.save(produto);
+	}
+	public void apagarPorId (Long id) {
+		produtoRepository.deleteById(id);
+	}
+	public void atualizarProduto(AtualizacaoProduto dados) {
+	    Produto produto = produtoRepository.findById(dados.id())
+	        .orElseThrow(() -> new EntityNotFoundException("Produto não encontrado"));
+	    produto.atualizarInformacoes(dados);
+	}
+
+  
+}
