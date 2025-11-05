69ab457f38ce2b78fa0526787dfb7db84032d10f
LuquinhasDoJava <143123619+LuquinhasDoJava@users.noreply.github.com>
2025-11-05 00:40:18 -0300

    Delete src directory

--- src/main/java/com/example/frota/marca/CadastroMarca.java
+++
@@ -1,9 +0,0 @@
-package com.example.frota.marca;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record CadastroMarca(
-        @NotBlank String nome,
-        @NotBlank String pais
-) {
-}
